77cb5db3bcefd0093e6fbaddacf636b8e7ba14cb
pauline_grace <pedepolonia@up.edu.ph>
2021-07-03 15:14:17 +0800

    update index view for controllers

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,68 +1,48 @@
 class PlaylistsController < ApplicationController
   before_action :set_playlist, only: %i[ show edit update destroy ]
 
-  # GET /playlists or /playlists.json
   def index
     @playlists = Playlist.all
   end
 
-  # GET /playlists/1 or /playlists/1.json
   def show
   end
 
-  # GET /playlists/new
   def new
     @playlist = Playlist.new
   end
 
-  # GET /playlists/1/edit
   def edit
   end
 
-  # POST /playlists or /playlists.json
   def create
     @playlist = Playlist.new(playlist_params)
 
-    respond_to do |format|
-      if @playlist.save
-        format.html { redirect_to @playlist, notice: "Track was successfully created." }
-        format.json { render :show, status: :created, location: @playlist }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @playlist.errors, status: :unprocessable_entity }
-      end
+    if @playlist.save
+      redirect_to @playlist, notice: "Track was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /playlists/1 or /playlists/1.json
   def update
-    respond_to do |format|
-      if @playlist.update(playlist_params)
-        format.html { redirect_to @playlist, notice: "Track was successfully updated." }
-        format.json { render :show, status: :ok, location: @playlist }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @playlist.errors, status: :unprocessable_entity }
-      end
+    if @playlist.update(playlist_params)
+      redirect_to @playlist, notice: "Track was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity 
     end
   end
 
-  # DELETE /playlists/1 or /playlists/1.json
   def destroy
     @playlist.destroy
-    respond_to do |format|
-      format.html { redirect_to playlists_url, notice: "Track was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to playlists_url, notice: "Track was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_playlist
       @playlist = Playlist.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def playlist_params
       params.require(:playlist).permit(:song_title, :artist, :album)
     end
