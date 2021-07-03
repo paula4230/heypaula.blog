0d4aed44cc86b76cf5a09670570471011673e49f
pauline_grace <pedepolonia@up.edu.ph>
2021-07-03 21:47:02 +0800

    add validations for model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,5 @@
 class Playlist < ApplicationRecord 
     validates :song_title, :artist, presence: true
+    validates :song_title, uniqueness: true, on: :create
+    validates :song_title, uniqueness: true, on: :update 
 end
