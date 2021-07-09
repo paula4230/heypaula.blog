a5453264c2e42db1515b77ead84cc1d3a7792697
pauline_grace <pedepolonia@up.edu.ph>
2021-07-09 20:01:35 +0800

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :playlists
   root to: "playlists#index"
+
+  get '/playlists' => 'playlists#index', as: 'question_index'
+  get '/playlists/html' => 'playlists#html', as: 'playlist_html',
+  get '/playlists/css' => 'playlists#css', as: 'playlist_css',
+  get '/playlists/javascript' => 'playlists#javascript', as: 'playlist_javascript',
+  get '/playlists/ruby' => 'playlists#ruby', as: 'playlist_ruby',
 end
